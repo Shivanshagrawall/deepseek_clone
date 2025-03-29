4fdcb63ed2ef2d4d0a88f711f2aa079dc1571e20
Shivansh Agrawal <Shivanshagrawal3007@gmail.com>
2025-03-29 10:46:56 +0530

    fix bugs

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -2,7 +2,7 @@ import { Webhook } from "svix";
 import connectDB from "@/config/db";
 import User from "@/models/User";
 import { headers } from "next/headers";
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function POST(req){
     const wh=new Webhook(process.env.SIGNING_SECRET);
@@ -44,5 +44,5 @@ export async function POST(req){
             break;
     }
 
-    return NextRequest.json({message:"Event received"});
+    return NextResponse.json({message:"Event received"});
 }
--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ let cached=global.mongoose || {conn:null, promise:null}
 
 export default async function connectDB(){
     if(cached.conn) return cached.conn;
-    if(cached.promise){
+    if(!cached.promise){
         cached.promise=mongoose.connect(process.env.MONGODB_URI).then((mongoose)=>mongoose);
     }
     try {
