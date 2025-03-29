93695e4655a613bc8e44ecc6bd7974547240c989
Shivansh Agrawal <Shivanshagrawal3007@gmail.com>
2025-03-29 10:38:40 +0530

    updated

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -2,7 +2,7 @@ import { Webhook } from "svix";
 import connectDB from "@/config/db";
 import User from "@/models/User";
 import { headers } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
 export async function POST(req){
     const wh=new Webhook(process.env.SIGNING_SECRET);
@@ -44,5 +44,5 @@ export async function POST(req){
             break;
     }
 
-    return NextResponse.json({message:"Event received"});
+    return NextRequest.json({message:"Event received"});
 }
