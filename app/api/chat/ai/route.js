71215ebc5cb7e1128f55d8ea023b7f8d48a4175e
Shivansh Agrawal <Shivanshagrawal3007@gmail.com>
2025-03-29 12:40:09 +0530

    updated

---
+++ app/api/chat/ai/route.js
@@ -0,0 +1,56 @@
+export const maxDuration=60;
+import connectDB from "@/config/db";
+import Chat from "@/models/Chat";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+    baseURL: 'https://api.deepseek.com',
+    apiKey: process.env.DEEPSEEK_API_KEY,
+});
+
+export async function POST(req){
+    try {
+        const {userId}=getAuth(req);
+
+        // Extract the chatid and prompts from the request body
+        const {chatId, prompt} = await req.json();
+
+        if(!userId){
+            return NextResponse.json({
+                success:false,
+                message:"User not authenticate",
+            });
+        }
+
+        // Find the chat document in the database based on the userId and ChatId
+        await connectDB();
+        const data=await Chat.findOne({userId,_id:chatId})
+
+        // Craete a user messsage object
+        const userPrompt={
+            role:"user",
+            content:prompt,
+            timestamp:Date.now(),
+        };
+        data.messages.push(userPrompt);
+
+        // Call the deepseek api to get the chat completion
+
+        const completion = await openai.chat.completions.create({
+            messages: [{ role: "user", content: prompt }],
+            model: "deepseek-chat",
+            store:true,
+        });
+
+        const message=completion.choices[0].message;
+        message.timestamp=Date.now();
+        data.messages.push(message);
+        data.save();
+
+        return NextResponse.json({success:true, data:message})
+    } catch (error) {
+        return NextResponse.json({success:false,error:error.message});
+    }
+}
